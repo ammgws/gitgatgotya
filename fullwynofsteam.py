023e42c268e393efd494f9de9d5a07781faa7efc
Jason <jason@kayoway.com>
2017-10-12 00:41:52 +0900

    Use latest boilerplate from other project

--- fullwynofsteam.py
+++ fullwynofsteam.py
@@ -5,31 +5,58 @@ import datetime as dt
 import logging
 import os
 import os.path
+from configparser import ConfigParser
+from pathlib import Path
 from time import sleep
 # third party
 import click
 import requests
 from hangoutsclient import HangoutsClient
 
+APP_NAME = 'gitgatgotya'
+
+
+def create_dir(ctx, param, directory):
+    if not os.path.isdir(directory):
+        os.makedirs(directory, exist_ok=True)
+    return directory
+
 
 @click.command()
-@click.option('--config_path', '-c',
-              default=os.path.expanduser('~/.config/gitgatgotya'),
-              type=click.Path(exists=True),
-              help='path to directory containing config file.')
+@click.option(
+    '--config-path',
+    type=click.Path(),
+    default=os.path.join(os.environ.get('XDG_CONFIG_HOME', os.path.expanduser('~/.config')), APP_NAME),
+    callback=create_dir,
+    help='Path to directory containing config file. Defaults to XDG config dir.',
+)
+@click.option(
+    '--cache-path',
+    type=click.Path(),
+    default=os.path.join(os.environ.get('XDG_CACHE_HOME', os.path.expanduser('~/.cache')), APP_NAME),
+    callback=create_dir,
+    help='Path to directory to store logs and such. Defaults to XDG cache dir.',
+)
 @click.option('--steam-id', '-i', help='SteamID of the user to check.')
 @click.option('--api-key', '-k', help='Steam Web API key')
-def main(config_path, steam_id, api_key):
+def main(config_path, cache_path, steam_id, api_key):
     """
     Login to Hangouts, send generated message and disconnect.
     """
     configure_logging(config_path)
 
-    config_file = os.path.join(config_path, 'gat_steam.ini')
-    logging.debug('Using config file: %s', config_file)
     # TODO: see if can get click to load config file as fallback
     # See default_map (Context Defaults) or click-configfile
     # api_key = config.get('Hangouts', 'refresh_token')
+    config_file = os.path.join(config_path, 'gat_steam.ini')
+    config = ConfigParser()
+    config.read(config_file)
+    logging.debug('Using config file: %s', config_file)
+    hangouts_client_id = config.get('Hangouts', 'client_id')
+    hangouts_client_secret = config.get('Hangouts', 'client_secret')
+    hangouts_token_file = os.path.join(cache_path, 'hangouts_cached_token')
+    if not os.path.isfile(hangouts_token_file):
+        Path(hangouts_token_file).touch()
 
     # Get currently played game, if any
     url = f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={api_key}&format=json&steamids={steam_id}"
@@ -41,8 +68,7 @@ def main(config_path, steam_id, api_key):
         else:
             return
 
-    # Setup Hangouts bot instance, connect and send message
-    hangouts = HangoutsClient(config_file)
+    hangouts = HangoutsClient(hangouts_client_id, hangouts_client_secret, hangouts_token_file)
     if hangouts.connect():
         hangouts.process(block=False)
         sleep(5)  # need time for Hangouts roster to update
